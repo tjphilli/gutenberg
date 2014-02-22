2c9cecfdd91ea97cee8113a41a069df400b7972c
Trevor Phillippi <trevorphillippi@timberline-25.dynamic2.rpi.edu>
2014-02-22 11:46:10 -0500

    Adds basic functionality to alter leading of a given block of text

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,21 @@
 angular.module('gutenbergApp')
   .controller('MainController', function ($scope) {
     $scope.test = "Something";
+
+    $scope.type = {
+    	leading: {
+    		value: 1,
+    		increase: function(){
+    			this.value +=1
+    		},
+    		decrease: function(){
+    			this.value -=1
+    		}
+    	},
+    	size: {
+    		value: 16,
+    		unit: "px"
+    	}
+    };
+
   });
