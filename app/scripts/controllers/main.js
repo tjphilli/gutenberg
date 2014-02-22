d2ec58ec46c7725a91b60bb8ca86fba2778b1240
Trevor Phillippi <trevorphillippi@timberline-25.dynamic2.rpi.edu>
2014-02-22 12:25:54 -0500

    Adds functionality to increment and decrement size

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,18 +5,40 @@ angular.module('gutenbergApp')
     $scope.test = "Something";
 
     $scope.type = {
+    	test: 5,
     	leading: {
     		value: 1,
+    		test: $scope.type,
     		increase: function(){
     			this.value +=1
     		},
     		decrease: function(){
     			this.value -=1
+    		},
+    		microIncrease: function() {
+				this.value = parseFloat((this.value +=0.05).toFixed(2));
+				console.log(this);
+    		},
+    		microDecrease: function() {
+    			this.value = parseFloat((this.value -=0.05).toFixed(2))
     		}
     	},
     	size: {
     		value: 16,
-    		unit: "px"
+    		unit: "px",
+    		increase: function(){
+    			this.value +=5
+    		},
+    		decrease: function(){
+    			this.value -=5
+    		},
+    		microIncrease: function() {
+				this.value +=1
+				console.log(this);
+    		},
+    		microDecrease: function() {
+    			this.value -=1
+    		}
     	}
     };
 
