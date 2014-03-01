95f36b13304c448750e6e8d8f29433e4cbf4305c
Trevor Phillippi <trevor@tjphilli.com>
2014-03-01 00:34:17 -0500

    Adds environment variables in Angular via grunt

--- app/scripts/services/PlaceholderTextService.js
+++ app/scripts/services/PlaceholderTextService.js
@@ -1,7 +1,13 @@
 // Service to store and retrieve application wide data
-app.factory('PlaceholderTextService', ['$http', '$q', function($http, $q){
+app.factory('PlaceholderTextService', ['$http', '$q', 'ENV',function($http, $q, ENV){
+	URL_BASE = "";
+	if (ENV === 'development') {
+		URL_BASE = "http://localhost:3000/";
+	}
+	else if (ENV === 'production') {
+		URL_BASE = "/";	
+	}
 	return {
-
 		getLoremIpsum: function(num){
 		   return $http.jsonp("http://loripsum.net/api/" + num);
 		},
@@ -9,7 +15,7 @@ app.factory('PlaceholderTextService', ['$http', '$q', function($http, $q){
 		   return $http.jsonp("http://hipsterjesus.com/api/");
 		},
 		getLocalText: function(num) {
-			return $http.get("http://localhost:3000/api/"+num);
+			return $http.get(URL_BASE +"api/"+num);
 		}
 	}
 
