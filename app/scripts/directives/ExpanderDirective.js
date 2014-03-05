0073212f3674966eaae1c88b2e00d232e7576d48
Trevor Phillippi <trevor@tjphilli.com>
2014-03-05 03:26:02 -0500

    Changes expander modules to animate

--- app/scripts/directives/ExpanderDirective.js
+++ app/scripts/directives/ExpanderDirective.js
@@ -1,16 +1,22 @@
 app.directive('expander', function () {
     return {
         link: function (scope, element, attrs) {
-           var $target = $(element).children('.expander-target:not(.expander .expander .expander-trigger)').addClass('is-hiding');
-           var $target2 = $(element).children('.expander-target').addClass('is-hiding');
+           var $target = $(element).children('.expander-target:not(.expander .expander .expander-trigger)').addClass('is-collapsed');
+           var $target2 = $(element).children('.expander-target').addClass('is-collapsed');
            $(element).on('click', '.expander-trigger:not(.expander .expander .expander-trigger)', function(){
-                $target.toggleClass('is-hiding');
+                $target.toggleClass('is-collapsed');
+                scope.$apply()
                 console.log("lcikc 1");
            })
            $(element).on('click', '.expander .expander-trigger', function(){
-                $(element).children('.expander-target').toggleClass('is-hiding');
+                $(element).children('.expander-target').toggleClass('is-collapsed');
+                scope.$apply()
                 console.log("lcikc 2");
            })
+           $(element).children('.properties-menu').on('click', function(){
+           		console.log("messsagegfdgsfd");
+           		$target.toggleClass('is-collapsed');
+           });
         }
     };
 });
