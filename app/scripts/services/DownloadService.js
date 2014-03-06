abddd96d0c3c8ea5a8dc348d7d4efb9a75253f00
Trevor Phillippi <trevor@tjphilli.com>
2014-03-06 02:25:54 -0500

    Creates a Globals service to detect environment

--- app/scripts/services/DownloadService.js
+++ app/scripts/services/DownloadService.js
@@ -1,11 +1,8 @@
 // Service to store and retrieve application wide data
-app.factory('DownloadService', ['$http', '$q', function($http, $q){
+app.factory('DownloadService', ['$http', '$q', 'Globals', function($http, $q, Globals){
 	return {
 		getDownload: function(data) {
-			return $http.post("http://localhost:3000/dl/", data);
-		},
-		getDl: function(data) {
-			return $http.get("http://localhost:3000/dl/" + data);
+			return $http.post(Globals.urlBase() + "dl/", data);
 		}
 	}
 
