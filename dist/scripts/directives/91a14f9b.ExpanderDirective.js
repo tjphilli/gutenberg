00506adbc62940f03410fb18e3ed4e183824c76b
Trevor Phillippi <trevor@tjphilli.com>
2014-03-06 03:28:53 -0500

    Builds app

--- dist/scripts/directives/91a14f9b.ExpanderDirective.js
+++ dist/scripts/directives/91a14f9b.ExpanderDirective.js
@@ -3,12 +3,14 @@ app.directive('expander', function () {
         link: function (scope, element, attrs) {
            var $target = $(element).children('.expander-target:not(.expander .expander .expander-trigger)').addClass('is-collapsed');
            var $target2 = $(element).children('.expander-target').addClass('is-collapsed');
-           $(element).on('click', '.expander-trigger:not(.expander .expander .expander-trigger)', function(){
+           $(element).on('click', '.expander-trigger:not(.expander .expander .expander-trigger)', function(e){
+                $(e.target).closest('.expander-trigger').toggleClass('is-triggered')
                 $target.toggleClass('is-collapsed');
                 scope.$apply()
                 console.log("lcikc 1");
            })
-           $(element).on('click', '.expander .expander-trigger', function(){
+           $(element).on('click', '.expander .expander-trigger', function(e){
+                $(e.target).closest('.expander-trigger').toggleClass('is-triggered')
                 $(element).children('.expander-target').toggleClass('is-collapsed');
                 scope.$apply()
                 console.log("lcikc 2");
